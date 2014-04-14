9e29390b070e9423dbfea59be2fea3d480398538
Kákonyi Roland <roland@webstar.hu>
2014-04-14 11:43:58 +0200

    Fix build tool selection

--- start.sh
+++ start.sh
@@ -4,16 +4,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 BUILDTOOL="make"
-IS_OSX=`uname -a|grep Darwin`
 
-if [ IS_OSX ]; then
+IS_OSX=`command -v xcodebuild 1> /dev/null`
+
+if [ -x IS_OSX ] ; then
     BUILDTOOL="xcodebuild"
 fi
 
 ${BUILDTOOL} clean
 ${BUILDTOOL}
 
-if [ IS_OSX ]; then
+if [ -x IS_OSX ]; then
     rm -rf obj
     cp -R build/Release obj
     chmod a+x obj/player
